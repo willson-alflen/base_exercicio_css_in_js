cb6a72b029a1346a96ab019624d93a362dc89d3e
willson-alflen <willson.alflen@gmail.com>
2023-08-16 06:33:20 -0300

    modify Hero to use styled components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { StyledForm } from './Hero'
 
 const Hero = () => (
-  <form className={styles.form}>
+  <StyledForm>
     <div className="container">
-      <h2 className={styles.heroTitle}>
+      <h2 className="heroTitle">
         As melhores vagas para tecnologia, design e artes visuais.
       </h2>
     </div>
-  </form>
+  </StyledForm>
 )
 
 export default Hero
