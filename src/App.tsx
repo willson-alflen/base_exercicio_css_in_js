df989ce664f0c83cc9a5429acdde1014dbc2c04a
willson-alflen <willson.alflen@gmail.com>
2023-08-16 05:53:09 -0300

    modify global.css and Cabecalho.module.css to use styled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import { Container, GlobalStyles } from './GlobalStyles'
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,9 @@
-import styles from './Cabecalho.module.css'
+import { StyledHeader } from './Cabecalho'
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
+  <StyledHeader>
     <h1>EBAC Jobs</h1>
-  </header>
+  </StyledHeader>
 )
 
 export default Cabecalho
